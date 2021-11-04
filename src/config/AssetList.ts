d16a924ef7b1505169de6ae82378a07d1116847d
Steve Xu <steve.x@gen-game.com>
2021-11-04 14:31:20 +0800

    [bugfix] Fix import

--- src/config/AssetList.ts
+++ src/config/AssetList.ts
@@ -1,8 +1,8 @@
 export const list: Array<IAsset> = [
-	// {
-	// 	assetKey: 'example.png',
-	// 	assetUrl: 'assets/example.png'
-	// }
+	{
+		assetKey: 'example.png',
+		assetUrl: 'assets/example.png'
+	}
 ];
 
 export interface IAsset {
