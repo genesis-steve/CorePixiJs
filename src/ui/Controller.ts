cd1b8bd7ca99f10f469189156b4415e00501ff9f
Steve Xu <steve.x@gen-game.com>
2021-11-10 11:41:22 +0800

    [Listeners] Add keyboard and gamepad listners as default

--- src/ui/Controller.ts
+++ src/ui/Controller.ts
@@ -1,7 +1,16 @@
+import { Inject } from 'typescript-ioc';
 import { Config, IConfig } from 'core/src/ui/Config';
 import { View } from 'core/src/ui/View';
+import { IKeyboardDownEventData, IKeyboardPressEventData, KeyboardManager } from 'core/src/core/KeyboardManager';
+import { GamePadManager, IGamePadAxesEventData, IGamePadButtonEventData } from 'core/src/core/GamePadManager';
 
 export class Controller implements IController {
+
+	@Inject
+	protected keyboardManager: KeyboardManager;
+
+	@Inject
+	protected gamePadManager: GamePadManager;
 
 	protected config: IConfig;
 	protected view: View;
@@ -17,6 +26,49 @@ export class Controller implements IController {
 	protected init (): void {
 		this.config = new Config();
 		this.view = new View( this.config );
+		this.addListeners();
+	}
+
+	protected addListeners (): void {
+		this.keyboardManager.onKeyDownSignal.add( this.onKeyDown, this );
+		this.keyboardManager.onKeyPressSignal.add( this.onKeyPress, this );
+		this.keyboardManager.onKeyUpSignal.add( this.onKeyUp, this );
+		this.gamePadManager.onButtonUpdateSignal.add( this.onGamePadButtonUpdate, this );
+		this.gamePadManager.onAxesUpdateSignal.add( this.onGamePadAxesUpdate, this );
+	}
+
+	protected onKeyDown ( data: IKeyboardDownEventData ): void {
+		switch ( data.code ) {
+			//
+		}
+	}
+
+	protected onKeyUp ( data: IKeyboardDownEventData ): void {
+		switch ( data.code ) {
+			//
+		}
+	}
+
+	protected onKeyPress ( data: IKeyboardPressEventData ): void {
+		for ( let i: number = 0; i < data.buttons.length; i++ ) {
+			// const code: string = data.buttons[ i ].code;
+		}
+	}
+
+	protected onGamePadButtonUpdate ( data: IGamePadButtonEventData ): void {
+		data.buttons.forEach( button => {
+			switch ( button.key ) {
+				//
+			}
+		} );
+	}
+
+	protected onGamePadAxesUpdate ( data: IGamePadAxesEventData ): void {
+		data.axes.forEach( axes => {
+			switch ( axes.key ) {
+				//
+			}
+		} );
 	}
 
 	public updateFrame (): void {
